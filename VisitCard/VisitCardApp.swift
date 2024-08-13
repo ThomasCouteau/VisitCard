493e1f41ead332d719a8b737632fee79c2436154
ThomasCouteau <mrthomas.couteau@gmail.com>
2024-08-13 21:42:58 +0200

    Initial Commit

---
+++ VisitCard/VisitCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitCardApp.swift
+//  VisitCard
+//
+//  Created by Thomas Couteau on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct VisitCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
